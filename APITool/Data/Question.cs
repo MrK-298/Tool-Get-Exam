65f86a89d15574bc943af3fa1a462bbdf5424512
huyho <huyho@LAPTOP-D3ASDEVB>
2024-04-16 22:23:56 +0700

    create data

---
+++ APITool/Data/Question.cs
@@ -0,0 +1,24 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.Data
+{
+    public class Question
+    {
+        public ObjectId Id { get; set; }
+        public string type { get; set; }
+        public string questionText { get; set; }
+        [BsonIgnoreIfNull]
+        public List<Answer> Answers { get; set; }
+        [BsonIgnoreIfNull]
+        public string imageUrl { get; set; }
+        [BsonIgnoreIfNull]
+        public List<SubQuestion> SubQuestions { get; set; }
+    }
+
+}
