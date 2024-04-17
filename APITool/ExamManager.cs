022b6944d02461c689da1dcc401bab664b652530
MrK-298 <113235309+MrK-298@users.noreply.github.com>
2024-04-17 14:29:59 +0700

    addquestion

--- APITool/ExamManager.cs
+++ APITool/ExamManager.cs
@@ -24,5 +24,18 @@ namespace APITool
             var exam = _examCollection.Find(filter).FirstOrDefault();
             return exam != null;
         }
+        public Exam findExamById(ObjectId examId)
+        {
+            var filter = Builders<Exam>.Filter.Eq("_id", examId);
+            var exam = _examCollection.Find(filter).FirstOrDefault();
+            return exam;
+        }
+        public void AddQuestionToExam(Question question, ObjectId examId)
+        {
+            var filter = Builders<Exam>.Filter.Eq("_id", examId);
+            var update = Builders<Exam>.Update.Push("Questions", question);
+            _examCollection.UpdateOne(filter, update);
+            questionId = question.Id;
+        }
     }
 }
