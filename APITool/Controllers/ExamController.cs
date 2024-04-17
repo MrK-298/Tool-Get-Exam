ac82b60279418f961a0def7ef35817f1c8e93104
MrK-298 <113235309+MrK-298@users.noreply.github.com>
2024-04-17 10:13:42 +0700

    connect database

---
+++ APITool/Controllers/ExamController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace APITool.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ExamController : ControllerBase
+    {
+
+    }
+}
---
+++ APITool/ExamManager.cs
@@ -0,0 +1,28 @@
+﻿using APITool.Data;
+using MongoDB.Bson;
+using MongoDB.Driver;
+
+namespace APITool
+{
+    public class ExamManager
+    {
+        private readonly IMongoCollection<Exam> _examCollection;
+        public ObjectId questionId;
+        public ExamManager()
+        {
+            var client = new MongoClient("mongodb+srv://khoinguyen29082002:khoibia123@hoangkhoi.9ehzu5m.mongodb.net/");
+            var database = client.GetDatabase("WebTiengAnh");
+            _examCollection = database.GetCollection<Exam>("Exam");
+        }
+        public void AddExam(Exam exam)
+        {
+            _examCollection.InsertOne(exam);
+        }
+        public bool IsExamExists(string examName)
+        {
+            var filter = Builders<Exam>.Filter.Eq(x => x.name, examName);
+            var exam = _examCollection.Find(filter).FirstOrDefault();
+            return exam != null;
+        }
+    }
+}
