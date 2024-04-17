1c89632a899ba304884503574dd1309578e18620
NQTuoG <112391190+NQTuoG@users.noreply.github.com>
2024-04-17 09:55:34 +0700

    findexam

---
+++ APITool/Function/FindExam.cs
@@ -0,0 +1,30 @@
+﻿using OpenQA.Selenium;
+
+namespace APITool.Function
+{
+    public class FindExam
+    {
+        public IWebDriver updateDriver;
+        public void GoToExam(IWebDriver driver)
+        {
+            IWebElement button1 = driver.FindElement(By.XPath("//*[@id=\"main-content\"]/div[2]/div/div/div[1]/div[1]/a"));
+            button1.Click();
+            Thread.Sleep(1000);
+            IWebElement button2 = driver.FindElement(By.XPath("//*[@id=\"exam-modal_notice\"]/div/div/div[2]/div[2]/a"));
+            button2.Click();
+            Thread.Sleep(1000);
+            var emailInput = driver.FindElement(By.XPath("//*[@id=\"login-box\"]/div/div[2]/div/div[1]/form/div/div[1]/input"));
+            emailInput.SendKeys("binbb1324@gmail.com");
+            var passwordInput = driver.FindElement(By.XPath("//*[@id=\"login-box\"]/div/div[2]/div/div[1]/form/div/div[2]/input"));
+            passwordInput.SendKeys("khoibia123");
+            Thread.Sleep(2000);
+            IWebElement button3 = driver.FindElement(By.XPath("//*[@id=\"login-box\"]/div/div[2]/div/div[1]/form/div/div[3]/button"));
+            button3.Click();
+            Thread.Sleep(3000);
+            IWebElement button4 = driver.FindElement(By.XPath("//*[@id=\"main-content\"]/div[2]/div/div/div[1]/div[1]/a"));
+            button4.Click();
+            updateDriver = driver;
+        }
+    }
+
+}
